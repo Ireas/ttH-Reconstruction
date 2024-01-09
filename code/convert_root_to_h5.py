d69060fbfab232b9f4fdf129cfba798840983368
Ireas Tom Raschke <ireasraschke@gmail.com>
2024-01-09 19:24:35 +0100

    improved conversion to SPANet format

--- code/convert_root_to_h5.py
+++ code/convert_root_to_h5.py
@@ -3,14 +3,15 @@ import uproot # root in python
 import h5py # h5 in python
 import awkward # working with irregular arrays
 import numpy as np
+import time
 
 # ==========  CONVERT .root TO .h5 FILES  ==========
 # ==================================================
 # converts .root file to a .h5 file at same destination and with same name
 # todo: improve speed (mutlithreading, non-uniform data, ...)
 # todo: take input from argument as print function
-# todo: add timestamps to output estimated time
 
+TESTING_MODE = True
 ROOT_FILE_DESTINATION = "../output/rdataframes_output.root"
 H5_FILE_DESTINATION = "../output/output.h5"
 
@@ -23,12 +24,16 @@ def main():
 
 def convert_to_h5(root_file):
 	h5_file = h5py.File(H5_FILE_DESTINATION, 'w')
+	
+	start = time.time()
 
 	number_of_events = len(root_file['matched/nJets'].array())
 	max_number_of_jets = max(root_file['matched/nJets'].array())
-	print(number_of_events)
-	print(max_number_of_jets)
+	if TESTING_MODE and number_of_events>100:
+		number_of_events = 100
 
+	
+	print("creating group 'INPUTS'")
 	input_group = h5_file.create_group("INPUTS")
 	source_group = input_group.create_group("SOURCE")
 	mask = source_group.create_dataset("MASK", (number_of_events,max_number_of_jets), dtype=bool)
@@ -37,10 +42,12 @@ def convert_to_h5(root_file):
 	jet_eta = source_group.create_dataset("JET_ETA", (number_of_events,max_number_of_jets), dtype=float)
 	jet_phi = source_group.create_dataset("JET_PHI", (number_of_events,max_number_of_jets), dtype=float)
 	jet_e = source_group.create_dataset("JET_E", (number_of_events,max_number_of_jets), dtype=float)
-
 		
-	print("group: SOURCE")
-	for i in range(10):
+	step = 1/number_of_events
+	ratio = 0.0
+	displayed_percentage = 0
+
+	for i in range(number_of_events):
 		number_of_jets = root_file['matched/nJets'].array()[i]
 		for j in range(number_of_jets):
 			# important that both indicies are used simultanously, otherwise data is set in copied array and is discarded!
@@ -51,8 +58,15 @@ def convert_to_h5(root_file):
 			jet_e[i,j] = root_file['matched/jet_e_NOSYS'].array()[i,j]
 			jet_match_mask[i,j] = root_file['matched/jet_match_mask'].array()[i,j]
 
+		ratio+= step
+		if ratio>=0.1:
+			displayed_percentage+= 1
+			ratio-= 0.1
+			partial = time.time()
+			print("  >", 10*displayed_percentage, "% (estimated remaining time: ", round((partial-start)/displayed_percentage*(10-displayed_percentage)), "s)")
 	
-	print("group: TARGET")
+	print()	
+	print("creating group 'TARGET'")
 	target_group = h5_file.create_group("TARGETS")
 	t1_group = target_group.create_group("t1")
 	b1 = t1_group.create_dataset("b", (number_of_events), dtype=int)
@@ -66,23 +80,10 @@ def convert_to_h5(root_file):
 	
 	
 	h5_file.close()
-	exit()
-	
-#for branch in root_file['source'].keys():
-#		print("dataset: " + branch)
-#		content = root_file['source'][branch].array()
-#
-#		dt = h5py.special_dtype(vlen=np.dtype(float)) # use variable length float array because jet number varies per event
-#		dataset = source_group.create_dataset(branch, (len(content),), dtype=dt)
-#		for i, entry in enumerate(content): # add each event one-by-one
-#			dataset[i] = np.array(entry, dtype=float)
-#			if not mask_is_set:
-#				for j in range(len(entry)):	
-#					mask[i][j] = True		
-#		
-#		mask_is_set = True
-#		print("successfully written: " + branch + " to SOURCE")
-	
+	end = time.time()
+	print()
+	print("Time needed: ", round(end-start), "s for ", number_of_events, "events")
+		
 
 
 if __name__ == '__main__':
